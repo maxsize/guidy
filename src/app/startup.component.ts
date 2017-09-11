2230f083158df06cbaa88132dcd6029ad6054f41
Max <maxsizen@gmail.com>
2017-09-11 19:56:23 +0800

    init commit

---
+++ src/app/startup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core/core";
+
+@Component({
+    selector: 'startup',
+    templateUrl: './startup.component.html',
+    styleUrls: ['./startup.component.css']
+})
+
+export class StartupComponent
+{
+}
