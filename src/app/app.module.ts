ca4fd90af3836fbfe28bccc61496e060950cc30b
Max <maxsizen@gmail.com>
2017-09-12 21:03:06 +0800

    add attribute service and component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,17 @@ import { CountryService } from './service/country.service';
 import { AppRoutingModule } from './app-routing.module';
 import { CountriesComponent } from './countries.component';
 import { CountryComponent } from './country.component';
+import { AttractionService } from './service/attraction.service';
+import { AttractionComponent } from './attraction.component';
 
 @NgModule({
   declarations: [
-    AppComponent, StartupComponent, CountriesComponent, CountryComponent
+    AppComponent, StartupComponent, CountriesComponent, CountryComponent, AttractionComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule
   ],
-  providers: [ UserService, CountryService ],
+  providers: [ UserService, CountryService, AttractionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/attraction.component.ts
+++ src/app/attraction.component.ts
@@ -1,11 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Attraction } from "./data/Attraction";
+import { AttractionService } from "./service/attraction.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'attraction',
     templateUrl: './attraction.component.html'
 })
-export class AttractionComponent
+export class AttractionComponent implements OnInit
 {
-    att:Attraction;
+    att: Attraction;
+
+    constructor(private service:AttractionService, private route:ActivatedRoute){}
+
+    ngOnInit(): void {
+        // this.route.paramMap
+        // this.service.getAttraction()
+    }
 }
